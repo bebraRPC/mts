bc6b92da61c5c8d49ad0f8df22eafe5c3b8c4f0a
menyasosali <tapakah9@inbox.ru>
2023-07-02 09:47:30 +0300

    changed defaultAddr

--- pkg/httpserver/server.go
+++ pkg/httpserver/server.go
@@ -12,7 +12,7 @@ import (
 const (
 	_defaultReadTimeout     = 5 * time.Second
 	_defaultWriteTimeout    = 5 * time.Second
-	_defaultAddr            = ":8085"
+	_defaultAddr            = ":8080"
 	_defaultShutdownTimeout = 3 * time.Second
 )
 
