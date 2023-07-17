a3b2bc6dff9ef4757db0389a92be64192c1f125b
menyasosali <tapakah9@inbox.ru>
2023-07-17 10:19:37 +0300

    add log info for check

--- internal/service/kafka/consumer.go
+++ internal/service/kafka/consumer.go
@@ -25,17 +25,18 @@ type ImageConsumer struct {
 func NewImageConsumer(ctx context.Context, logger logger.Interface, processor ImageProcessor, cfg config.KafkaConfig,
 ) (*ImageConsumer, error) {
 	config := sarama.NewConfig()
+	config.Consumer.Return.Errors = true
 
-	consumer, err := sarama.NewConsumerGroup(cfg.Brokers, cfg.GroupID, config)
+	group, err := sarama.NewConsumerGroup(cfg.Brokers, cfg.GroupID, config)
 	if err != nil {
-		return nil, fmt.Errorf("failed to create Kafka consumer: %w", err)
+		return nil, fmt.Errorf("failed to create Kafka group: %w", err)
 	}
 
 	imageConsumer := &ImageConsumer{
 		Ctx:       ctx,
 		Logger:    logger,
 		Processor: processor,
-		Consumer:  consumer,
+		Consumer:  group,
 		Cfg:       cfg,
 	}
 
@@ -48,6 +49,7 @@ type imageConsumerHandler struct {
 }
 
 func (c *ImageConsumer) Start() {
+	c.Logger.Info("Start - consumer.go - 51")
 	go c.Consume()
 }
 
@@ -62,13 +64,16 @@ func (c *ImageConsumer) Close() error {
 }
 
 func (c *ImageConsumer) Consume() {
+	c.Logger.Info("67 - consumer.go - Consume() - start")
 	consumerHandler := imageConsumerHandler{
 		logger:    c.Logger,
 		processor: c.Processor,
 	}
 
-	for c.Ctx.Err() != nil {
+	c.Logger.Info("73 - consumer.go - Consume() - consumerHandler: ", consumerHandler)
+	for c.Ctx.Done() == nil {
 		err := c.Consumer.Consume(c.Ctx, []string{c.Cfg.Topic}, consumerHandler)
+		c.Logger.Info("75 - consumer.go - Consume() - After c.Consume.Consume(): ", consumerHandler)
 		if err != nil {
 			c.Logger.Error(fmt.Sprintf("Failed to consume Kafka message: %v", err))
 		}
@@ -79,6 +84,7 @@ func (h imageConsumerHandler) Setup(_ sarama.ConsumerGroupSession) error   { ret
 func (h imageConsumerHandler) Cleanup(_ sarama.ConsumerGroupSession) error { return nil }
 
 func (h imageConsumerHandler) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
+	h.logger.Info(fmt.Sprintf("imgKafka - consumer.go - ConsumerClaim - start"))
 	for message := range claim.Messages() {
 		imgKafka, err := extractImageInfo(message.Value)
 		if err != nil {
@@ -90,6 +96,7 @@ func (h imageConsumerHandler) ConsumeClaim(session sarama.ConsumerGroupSession, 
 		h.processor.ProcessImage(imgKafka)
 
 		session.MarkMessage(message, "")
+
 	}
 
 	return nil
